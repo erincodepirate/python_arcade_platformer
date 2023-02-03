1aad278380d3e0a1f99fa67dff7a2d126f0a9df2
Erin Clark <erincodepirate@gmail.com>
2023-02-03 15:34:40 -0800

    Add sprites

--- main.py
+++ main.py
@@ -4,17 +4,48 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
 
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+
 class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
+        self.wall_list = None
+        self.player_list = None
+
     def setup(self):
-        pass
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+
+        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(
+                ":resources:images/tiles/grassMid.png", 
+                TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", 
+                TILE_SCALING)
 
     def on_draw(self):
         self.clear()
+
+        self.wall_list.draw()
+        self.player_list.draw()
 
 def main():
     window = MyGame()
